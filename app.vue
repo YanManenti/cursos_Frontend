2cf96189b04e14bc1858de688e86530e76f97890
YanManenti <y.a.martins@outlook.com>
2024-07-01 19:25:36 -0300

    why god

--- app.vue
+++ app.vue
@@ -1,6 +1,55 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Button from '@/components/ui/button/Button.vue';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+const count = ref<number>(0)
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-white flex flex-col p-6">
+    <Button class="w-24" @click=" count++">Count is: {{ count }}</Button>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
+              <li class="row-span-3">
+                <NavigationMenuLink as-child>
+                  <a class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/">
+                    <img src="https://www.radix-vue.com/logo.svg" class="h-6 w-6">
+                    <div class="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p class="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   </div>
 </template>
